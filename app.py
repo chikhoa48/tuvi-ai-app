f070b0d46a81adb3606edb6e70012a6d09626933
chikhoa48 <chikhoa48@gmail.com>
2026-01-23 20:33:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import time
 import google.generativeai as genai
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
